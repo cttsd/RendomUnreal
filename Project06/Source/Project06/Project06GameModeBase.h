a962e8e36e47dab0e1744edde51c792d0d03cdf2
태훈 정 <c0t0h0@naver.com>
2023-06-16 11:38:09 +0900

    test

---
+++ Project06/Source/Project06/Project06GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Project06GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class PROJECT06_API AProject06GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
